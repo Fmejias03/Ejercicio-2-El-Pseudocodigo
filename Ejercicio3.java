c0325671a6b969776e4bc1585b884f085b0a05cd
Fernando Mejías Pando <113796719+Fmejias03@users.noreply.github.com>
2023-02-10 13:37:46 +0000

    Ejercicios 1-3

---
+++ Ejercicio3.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+import static java.lang.Double.parseDouble;
+
+public class Ejercicio3 {
+       /*¿Qué hace este algoritmo?
+
+Algo QueHace
+# ¿Qué hace este algoritmo?
+Variable valor1, valor2: real
+Inicio
+   valor1 <- enter("Introduza un valor: ")
+   valor2 <- enter("Introduza otro valor: ")
+   # tratamiento
+   valor1 <- valor2
+   valor2 <- valor1
+   escribir("valor1 = " & valor1 & "; valor2 = " & valor2)
+Fin */
+
+public static void main(String[] args) {
+    double valor1, valor2;
+    valor1 = parseDouble(JOptionPane.showInputDialog("Introduza un valor: "));
+    valor2 = parseDouble(JOptionPane.showInputDialog("Introduza otro valor: "));
+    // tratamiento
+    valor1 = valor2;
+    valor2 = valor1;
+    JOptionPane.showMessageDialog(null, "valor1 = " + valor1 + "; valor2 = " + valor2);
+}
+
+
+    
+}
